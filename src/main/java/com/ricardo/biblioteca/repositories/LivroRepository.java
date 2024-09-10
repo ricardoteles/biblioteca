5530489e4107e55b4565b8cd9a31ca8e8c77f559
C1331476 Ricardo Oliveira Teles <c1331476@interno.bb.com.br>
2024-09-10 14:32:23 -0300

    Criando os repositories

---
+++ src/main/java/com/ricardo/biblioteca/repositories/LivroRepository.java
@@ -0,0 +1,9 @@
+package com.ricardo.biblioteca.repositories;
+
+import com.ricardo.biblioteca.models.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Long> {
+}
---
+++ src/main/java/com/ricardo/biblioteca/repositories/PerfilRepository.java
@@ -0,0 +1,9 @@
+package com.ricardo.biblioteca.repositories;
+
+import com.ricardo.biblioteca.models.Perfil;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PerfilRepository extends JpaRepository<Perfil, Long> {
+}
